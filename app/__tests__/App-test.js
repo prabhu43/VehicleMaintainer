7a8ee98ef66ddbb7f1f85a96c0d860ccee6a8370
Prabhu Jayakumar <j.prabhu91@gmail.com>
2020-05-12 21:56:56 +0530

    Add snapshot test

--- app/__tests__/App-test.js
+++ app/__tests__/App-test.js
@@ -4,14 +4,15 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../app/App';
+import App from '../App';
 import Realm from 'realm';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 afterAll(() => {
