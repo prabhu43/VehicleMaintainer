e9abe72fd309a2aab8f432aef8562459d705aef0
Prabhu Jayakumar <j.prabhu91@gmail.com>
2020-05-12 20:06:18 +0530

    Setup jest with 1 test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,15 @@
 import 'react-native';
 import React from 'react';
 import App from '../app/App';
+import Realm from 'realm';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+afterAll(() => {
+  Realm.clearTestState();
 });
