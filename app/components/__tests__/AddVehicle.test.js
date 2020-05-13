d569a3937ad66161e2d7915db3c919df8aa3f7d6
Prabhu Jayakumar <j.prabhu91@gmail.com>
2020-05-14 01:19:33 +0530

    Add snapshot test for AddVehicle

---
+++ app/components/__tests__/AddVehicle.test.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import Realm from 'realm';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import AddVehicle from '../AddVehicle';
+
+it('renders correctly', () => {
+  let routeParams = { params: { mode: "create"}};
+  const tree = renderer.create(<AddVehicle route={routeParams}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+afterAll(() => {
+  Realm.clearTestState();
+});
